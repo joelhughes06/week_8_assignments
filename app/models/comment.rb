c0b0ae3fad03f893c8e05b854b5ff6f8e86deab9
Joel Hughes <joelhughes06@netscape.net>
2014-03-10 11:42:41 -0600

    initial commit

---
+++ app/models/comment.rb
@@ -0,0 +1,7 @@
+class Comment < ActiveRecord::Base
+  belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
+  belongs_to :post
+  has_many :post_categories
+  has_many :posts, through: :post_categories
+end
+
